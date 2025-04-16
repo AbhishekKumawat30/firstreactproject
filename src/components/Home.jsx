3c95ae6ad3fc71b01ec113328c009f94b732dcaa
Abhishek Kumawat <abhishek@gmail.com>
2025-04-16 15:48:16 +0530

    Updated App and added Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react"; // optional in React 17+
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h3>Hello friends</h3>
+      </div>
+    </>
+  );
+}
+
+export default Home;
